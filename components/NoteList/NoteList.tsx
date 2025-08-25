14a81d91ac7a1cef4d5ea420a4e957eeff6fd52c
Svitlana Shumal <svetlankachajka@gmail.com>
2025-08-25 14:25:04 +0300

    fixed path to notes details

--- components/NoteList/NoteList.tsx
+++ components/NoteList/NoteList.tsx
@@ -26,7 +26,7 @@ export default function NoteList({ notes }: NoteListProps) {
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
-            <Link href={"/notes/${note.id}"}>View details</Link>
+            <Link href={`/notes/${note.id}`}>View details</Link>
             <button
               className={css.button}
               onClick={() => mutation.mutate(note.id)}
